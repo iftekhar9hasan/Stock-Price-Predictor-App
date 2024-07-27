3c021033f2951caf0c69f2c29ab94dd513d7250a
Iftekhar Hasan <112223311+iftekhar9hasan@users.noreply.github.com>
2024-07-27 00:03:35 -0400

    Add files via upload

--- web_stock_price_predictor.py
+++ web_stock_price_predictor.py
@@ -2,7 +2,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from tensorflow.keras.models import load_model # type: ignore
+from tensorflow.keras.models import load_model
 import matplotlib.pyplot as plt
 import yfinance as yf
 
